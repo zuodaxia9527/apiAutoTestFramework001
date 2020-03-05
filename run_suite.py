c9537e6a8338487c4e47248ae8c7655903b9fdaa
Your Name <email@example>
2020-03-06 05:27:13 +0800

    修改后

--- run_suite.py
+++ run_suite.py
@@ -16,7 +16,8 @@ suite.addTest(unittest.makeSuite(TestTpshowlogin))
 suite.addTest(unittest.makeSuite(TestEmpLogin))
 suite.addTest(unittest.makeSuite(TestEmp))
 
-report_path = os.path.dirname(os.path.abspath(__file__)) + "/report/tpshop{}.html".format(time.strftime("%Y%m%d %H%M%S"))
+report_path = os.path.dirname(os.path.abspath(__file__)) + "/report/ihrm.html"
+#.format(time.strftime("%Y%m%d %H%M%S"))
 with open(report_path,"wb") as f:
     runner = HTMLTestRunner(f,verbosity=2,title="綜合测试",description="win10-chrome-80.0")
     runner.run(suite)
